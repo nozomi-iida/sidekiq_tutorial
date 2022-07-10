15a31fd050cdac9c2864391b69a325ef79a12a61
nozomi-iida <iida19990106@gmail.com>
2022-07-10 20:48:42 +0900

    fix: implement simple active job

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,5 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL'), id: nil }
-  config.log_formatter = Sidekiq::Logger::Formatters::JSON.new
+  config.redis = { url: ENV.fetch('REDIS_URL') }
 end
 
 Sidekiq.configure_client do |config|
